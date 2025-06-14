e6929379ea7e8bf5c2f4eddd7ccdb825d1860148
Haoran <dhr9936@gmail.com>
2025-06-14 21:14:19 +0100

    Update conf.py
    update the project file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SynthX'
+copyright = '2025, Haoran'
+author = 'Haoran'
 
 release = '0.1'
 version = '0.1.0'
